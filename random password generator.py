65abcae9081043ed9433bb73aeebfa203c3764f9
Salah <131046250+Suyoza@users.noreply.github.com>
2023-04-18 19:46:12 +0200

    Add files via upload
    This code generates a random password of a specified length using the random and string libraries in Python. It defines a function generate_password which takes a length argument and generates a random password by selecting characters randomly from a string containing letters, digits, and punctuation marks. The final password is returned by the generate_password function and can be displayed or stored for later use.

---
+++ random password generator.py
@@ -0,0 +1,12 @@
+import secrets
+import string
+
+def generate_password(length):
+    """Génère un mot de passe aléatoire avec une longueur donnée"""
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(chars) for i in range(length))
+    return password
+
+# Exemple d'utilisation : générer un mot de passe de 12 caractères
+password = generate_password(12)
+print(password)
